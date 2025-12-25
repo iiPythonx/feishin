eccb9ca7673f65b48ec3a83132a5ecef8695533e
iiPython <ben@iipython.dev>
2025-12-24 20:19:42 -0600

    pizza

--- src/renderer/store/settings.store.ts
+++ src/renderer/store/settings.store.ts
@@ -187,10 +187,10 @@ const DiscordSettingsSchema = z.object({
     clientId: z.string(),
     displayType: DiscordDisplayTypeSchema,
     enabled: z.boolean(),
+    enablePizza: z.boolean(),
     linkType: DiscordLinkTypeSchema,
     showAsListening: z.boolean(),
     showPaused: z.boolean(),
-    showServerImage: z.boolean(),
 });
 
 const FontSettingsSchema = z.object({
@@ -365,7 +365,6 @@ export const GeneralSettingsSchema = z.object({
     }),
     language: z.string(),
     lastFM: z.boolean(),
-    lastfmApiKey: z.string(),
     musicBrainz: z.boolean(),
     nativeAspectRatio: z.boolean(),
     passwordStore: z.string().optional(),
@@ -831,10 +830,10 @@ const initialState: SettingsState = {
         clientId: '1165957668758900787',
         displayType: DiscordDisplayType.FEISHIN,
         enabled: false,
+        enablePizza: true,
         linkType: DiscordLinkType.NONE,
         showAsListening: false,
         showPaused: true,
-        showServerImage: false,
     },
     font: {
         builtIn: 'Poppins',
@@ -868,7 +867,6 @@ const initialState: SettingsState = {
         },
         language: 'en',
         lastFM: true,
-        lastfmApiKey: '',
         musicBrainz: true,
         nativeAspectRatio: false,
         passwordStore: undefined,
