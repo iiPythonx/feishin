e2401df467e62cd48de315286805dd48ccf31a63
iiPython <ben@iipython.dev>
2025-12-24 20:19:42 -0600

    pizza

--- src/renderer/features/settings/components/window/discord-settings.tsx
+++ src/renderer/features/settings/components/window/discord-settings.tsx
@@ -10,7 +10,6 @@ import {
     DiscordDisplayType,
     DiscordLinkType,
     useDiscordSettings,
-    useGeneralSettings,
     useSettingsStoreActions,
 } from '/@/renderer/store';
 import { Select } from '/@/shared/components/select/select';
@@ -20,7 +19,6 @@ import { TextInput } from '/@/shared/components/text-input/text-input';
 export const DiscordSettings = memo(() => {
     const { t } = useTranslation();
     const settings = useDiscordSettings();
-    const generalSettings = useGeneralSettings();
     const { setSettings } = useSettingsStoreActions();
 
     const discordOptions: SettingOption[] = [
@@ -235,49 +233,23 @@ export const DiscordSettings = memo(() => {
         {
             control: (
                 <Switch
-                    checked={settings.showServerImage}
+                    checked={settings.enablePizza}
                     onChange={(e) => {
                         setSettings({
                             discord: {
-                                showServerImage: e.currentTarget.checked,
+                                ...settings,
+                                enablePizza: e.currentTarget.checked,
                             },
                         });
                     }}
                 />
             ),
-            description: t('setting.discordServeImage', {
+            description: t('setting.discordPizza', {
                 context: 'description',
-
-                discord: 'Discord',
                 postProcess: 'sentenceCase',
             }),
             isHidden: !isElectron(),
-            title: t('setting.discordServeImage', {
-                discord: 'Discord',
-                postProcess: 'sentenceCase',
-            }),
-        },
-        {
-            control: (
-                <TextInput
-                    defaultValue={generalSettings.lastfmApiKey}
-                    onBlur={(e) => {
-                        setSettings({
-                            general: {
-                                lastfmApiKey: e.currentTarget.value,
-                            },
-                        });
-                    }}
-                />
-            ),
-            description: t('setting.lastfmApiKey', {
-                context: 'description',
-                lastfm: 'Last.fm',
-                postProcess: 'sentenceCase',
-            }),
-            isHidden: !isElectron(),
-            title: t('setting.lastfmApiKey', {
-                lastfm: 'Last.fm',
+            title: t('setting.discordPizza', {
                 postProcess: 'sentenceCase',
             }),
         },
