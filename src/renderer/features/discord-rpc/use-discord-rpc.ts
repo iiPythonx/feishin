d3b99d696b572907c7a218d7d541a0e1e2e9667c
iiPython <ben@iipython.dev>
2025-12-24 21:31:44 -0600

    pizza fix

--- src/renderer/features/discord-rpc/use-discord-rpc.ts
+++ src/renderer/features/discord-rpc/use-discord-rpc.ts
@@ -41,7 +41,7 @@ export const useDiscordRpc = () => {
         id: currentSong?.imageId || undefined,
         imageUrl: currentSong?.imageUrl,
         itemType: LibraryItem.SONG,
-        type: 'table',
+        size: 100,
         useRemoteUrl: true,
     });
 
@@ -196,11 +196,11 @@ export const useDiscordRpc = () => {
                 }
 
                 // Handle pizza
-                if (trackChanged && song.imageUrl) {
+                if (trackChanged && imageUrl) {
                     const fetchedImage = await axios({
                         method: 'GET',
                         responseType: 'blob',
-                        url: song.imageUrl.replace(/&size=\d+/, '&size=100'),
+                        url: imageUrl,
                     });
 
                     // Send off to pizza
@@ -296,6 +296,7 @@ export const useDiscordRpc = () => {
             discordSettings.linkType,
             lastUniqueId,
             lastProxiedUrl,
+            imageUrl,
         ],
     );
 
