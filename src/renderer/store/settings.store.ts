810e6f3592fdb0d4ba7ecb0c10e867c667c0436b
iiPython <ben@iipython.dev>
2026-01-06 15:12:47 -0600

    turn off autoupdate by default

--- src/renderer/store/settings.store.ts
+++ src/renderer/store/settings.store.ts
@@ -1794,7 +1794,7 @@ const initialState: SettingsState = {
         type: 'audiomotionanalyzer',
     },
     window: {
-        disableAutoUpdate: false,
+        disableAutoUpdate: true,
         exitToTray: false,
         minimizeToTray: false,
         preventSleepOnPlayback: false,
